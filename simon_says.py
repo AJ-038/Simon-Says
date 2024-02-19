25d6eccac1d8a4de63384ffc184d55787446dbc7
Amanda <abe038@latech.edu>
2024-02-19 10:40:44 -0600

    message two!!!!!!!!!

--- simon_says.py
+++ simon_says.py
@@ -3,3 +3,43 @@ from time import sleep
 from random import choice
 import pygame
 from pygame.mixer import Sound
+
+pygame.init()
+
+GPIO.setmode(GPIO.LE_POTATO_LOOKUP)
+
+class Button:
+    
+    def __init__(self, switch:int, led:int, sound:str, color:str):
+        self.switch = switch
+        self.led = led
+        self.sound: Sound = Sound(sound)
+        self.color = color
+    
+
+    def setupGPIO(self):
+        GPIO.setup(self.switch, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+        GPIO.sestup(self.led, GPIO.OUT)
+
+    def turn_light_on(self):
+        GPIO.output(self.led, True)
+
+    def turn_light_off(self):
+        GPIO.output(self.led, False)
+
+    def is_pressed(self):
+        result = GPIO.input(self.switch)
+        if result == True:
+            return True
+        else:
+            return False
+
+    def respond(self):
+        pass
+
+    def __str__(self):
+        return self.color
+
+
+class Simon:
+    pass
